1e3522d5c88abb0b727b1f40ad669f91ade8b940
elvis huanca <JHuanca_21@outlook.com>
2023-07-16 15:47:12 -0500

    pagination styles

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -27,7 +27,7 @@ const Products = ({
   searchParams?: { page: string };
 }) => {
   const products = useSelector(selectAllProducts);
-  const status = useSelector(selectAllProductsStatus);
+  const productsStatus = useSelector(selectAllProductsStatus);
 
   const currentPage = searchParams?.page ? parseInt(searchParams?.page) : 1;
   const itemsPerPage = 10;
@@ -35,22 +35,23 @@ const Products = ({
   const maxItems = currentPage * itemsPerPage;
   const maxPages = Math.ceil(products.length / itemsPerPage);
   const items = products.slice(minItems, maxItems);
-  
+  const isEmtpy = items.length ? false : true;
+
   const dispatch = useAppDispatch();
   useEffect(() => {
     (async () => {
-      if (status === EStateGeneric.IDLE) {
+      if (productsStatus === EStateGeneric.IDLE) {
         await dispatch(getAllProducts());
       }
     })();
 
     return () => {
-      // dispatch(cleanUpProducts());
+      dispatch(cleanUpProducts());
     };
   }, []);
   return (
     <Layout>
-      {status === EStateGeneric.SUCCEEDED && (
+      {productsStatus === EStateGeneric.SUCCEEDED && !isEmtpy && (
         <div className="flex flex-col justify-between min-h-[80vh]">
           <div className="flex flex-wrap justify-center gap-4">
             {items.map((e, index) => (
@@ -60,12 +61,25 @@ const Products = ({
           <Pagination page={searchParams?.page} maxPages={maxPages} />
         </div>
       )}
-      {status === EStateGeneric.PENDING && (
+      {productsStatus === EStateGeneric.PENDING && (
         <div className="w-full h-[80vh]">
           <Loader />
         </div>
       )}
-      {status === EStateGeneric.FAILED && (
+      {productsStatus === EStateGeneric.SUCCEEDED && isEmtpy && (
+        <div className="flex flex-col justify-center items-center w-full h-[80vh]">
+          <div className="flex flex-col items-center p-5 bg-black/50 rounded-md">
+            <BiSolidError className="text-red-500 text-5xl sm:text-8xl" />
+            <h2 className="text-5xl md:text-6xl text-center font-bold">
+              Products not found
+            </h2>
+            <Link className="text-2xl text-red-600 underline" href="/">
+              Go to Home
+            </Link>
+          </div>
+        </div>
+      )}
+      {productsStatus === EStateGeneric.FAILED && (
         <div className="flex flex-col justify-center items-center w-full h-[80vh]">
           <div className="flex flex-col items-center p-5 bg-black/50 rounded-md">
             <BiSolidError className="text-red-500 text-5xl sm:text-8xl" />
--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,34 +1,45 @@
 "use client";
-import right from "@/assets/right.svg";
-import left from "@/assets/left.svg";
-import Image from "next/image";
-import Link from "next/link";
+import {
+  IoIosArrowDropleftCircle,
+  IoIosArrowDroprightCircle,
+} from "react-icons/io";
 type Props = {
   page: string | undefined;
   maxPages: number;
 };
+import { useRouter } from "next/navigation";
 
 const Pagination = ({ page, maxPages }: Props) => {
+  const router = useRouter();
+
   const currentPage = page ? parseInt(page) : 1;
+
+  const nextPage = () => {
+    router.push(`?page=${currentPage + 1}`);
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
+
+  const previousPage = () => {
+    router.push(`?page=${currentPage - 1}`);
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
   return (
     <div className="flex w-full justify-center items-center text-xl py-2 gap-2">
-      <Link
-        href={`${currentPage > 1 ? `?page=${currentPage - 1}` : `/products`}`}
+      <button
+        disabled={currentPage === 1}
+        onClick={previousPage}
+        className="disabled:opacity-50 disabled:hover:text-green-600 hover:text-green-500 text-green-600"
       >
-        <Image width={40} height={40} src={left} alt="left" loading="lazy" />
-      </Link>
-
-      <div>{currentPage}</div>
-
-      <Link
-        href={`${
-          currentPage < maxPages
-            ? `?page=${currentPage + 1}`
-            : `?page=${currentPage}`
-        }`}
+        <IoIosArrowDropleftCircle className="text-5xl" />
+      </button>
+      <span className="bg-green-600 px-4 py-2 rounded-full">{currentPage}</span>
+      <button
+        disabled={currentPage === maxPages}
+        onClick={nextPage}
+        className="disabled:opacity-50 disabled:hover:text-green-600 hover:text-green-500 text-green-600"
       >
-        <Image width={40} height={40} src={right} alt="right" loading="lazy" />
-      </Link>
+        <IoIosArrowDroprightCircle className="text-5xl" />
+      </button>
     </div>
   );
 };
